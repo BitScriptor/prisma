489e2421f4b2737018109a379c5835fc96bfcb52
bitScriptor <bit.scriptor@gmail.com>
2019-05-06 13:46:47 -0500

    Development of header
    Development of header component for all pages.

--- src/components/pages/header/header.jsx
+++ src/components/pages/header/header.jsx
@@ -3,18 +3,32 @@ import { Link } from "react-router-dom";
 import './header.css';
 
 class Header extends React.Component {
-  render() {
-    return (
-      <header>
-        <div className="text-center">
-          centro
-        </div>
-        <nav>
-          <Link to="/">Home</Link>
-        </nav>
-      </header>
-      );
-  }
+	render() {
+		return (
+			<header>
+				<div class="contactHeader">
+						<a href="#nuestroClientes">W (33) 11 36 13 74</a>
+						<a href="#servicios">T (33) 38 33 07 03 / (33) 36 56 01 01</a>
+				</div>
+				<div>
+					<img class="logoHeader" src={require("../images/logoPrisma.png")}/>
+				</div>
+				<nav className="text-center">
+					<hr/>
+					<div class="topnav">
+						<a class="active" href="#inicio">INICIO</a>
+						<a href="#nuestroClientes">NUESTROS CLIENTES</a>
+						<a href="#servicios">SERVICIOS</a>
+						<a href="#contacto">CONTACTO</a>
+					</div> 
+					<hr/>
+				</nav>
+				<div>
+					<Link to="/">Home</Link>
+				</div>
+			</header>
+		);
+	}
 }
 
 export default Header;
