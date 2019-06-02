fc52954b6582b7c6688793ade7d0d9450c155ecb
MarioSolano <msolano@espiralapp.com>
2019-06-01 21:25:25 -0500

    Slider autoplay

--- src/components/generals/slideImg/slideImg.jsx
+++ src/components/generals/slideImg/slideImg.jsx
@@ -19,7 +19,9 @@ var settings = {
 	speed: 500,
 	slidesToShow: 1,
 	slidesToScroll: 1,
-	focusOnSelect: false
+	focusOnSelect: false,
+	autoplay: true,
+  autoplaySpeed: 3000
 };
 
 class Slideshow extends React.Component {
